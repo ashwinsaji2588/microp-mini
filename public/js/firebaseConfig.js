cacb9a470b3817c540abac722248a35cda4a666a
Ashwin Saji <ashwinsaji2588@gmail.com>
2023-09-02 10:53:48 +0530

    comments added

--- public/js/firebaseConfig.js
+++ public/js/firebaseConfig.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyA-A21Vtjar89jAkdTY8bW-hnkoFOV_dp4",
-    authDomain: "mini-project-microp.firebaseapp.com",
-    databaseURL: "https://mini-project-microp-default-rtdb.firebaseio.com",
-    projectId: "mini-project-microp",
+    apiKey: "API KEY",
+    authDomain: "AUTH URL",
+    databaseURL: "URL",
+    projectId: "PROJECT ID",
     storageBucket: "mini-project-microp.appspot.com",
     messagingSenderId: "939049043573",
     appId: "1:939049043573:web:8a684d34943a55bfa21a3c",
